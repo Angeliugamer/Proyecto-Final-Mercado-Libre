8f3ef8cde2189820d407db7945c1af237fb22e9e
RAFA <RAFA@192.168.1.22>
2026-05-10 19:02:23 -0500

    10 de mayo de 2026

---
+++ src/Start/admin/AdminController.java
@@ -0,0 +1,40 @@
+package Start.admin;
+
+import Start.ConfiguracionGeneral;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class AdminController{
+    @FXML
+    private Pane overlay;
+    @FXML
+    private ImageView iconMoon;
+    @FXML
+    private void cambiarModo() {
+        if (!ConfiguracionGeneral.modoNoche) {
+            overlay.setOpacity(0.3);
+            iconMoon.setImage(new Image(
+                getClass().getResource("/img/Icons/Sun.png").toExternalForm()
+            ));
+            ConfiguracionGeneral.modoNoche = true;
+        } else {
+            overlay.setOpacity(0.0);
+            iconMoon.setImage(new Image(
+                getClass().getResource("/img/Icons/MoonStars.png").toExternalForm()
+            ));
+            ConfiguracionGeneral.modoNoche = false;
+        }
+    }
+    @FXML
+    public void initialize() {
+        if (ConfiguracionGeneral.modoNoche) {
+            overlay.setOpacity(0.3);
+            iconMoon.setImage(new Image(
+                getClass().getResource("/img/Icons/Sun.png").toExternalForm()
+            ));
+        }
+    }
+}
+
